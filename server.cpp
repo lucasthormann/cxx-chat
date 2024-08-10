d1ed0c8f77862d5a0602895827d1513ea25de74f
lucasthormann <LucasThormann@protonmail.com>
2024-08-10 15:03:52 -0500

    Function prototypes

---
+++ server.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <thread>
+#include <mutex>
+#define MAX_LEN 200
+#define NUM_COLORS 6
+
+using namespace std;
+
+struct terminal{
+  int id;
+  string name;
+  int socket;
+  thread th;
+};
+
+vector<terminal> clients;
+string def_col = "\033[0m";
+string colors[] = {"\033[31m", "\033[32m", "\033[33m", "\033[34m", "\033[35m", "\033[36m"};
+int seed = 0;
+mutex cout_mtx, clients_mtx;
+
+string color(int code);
+void set_name(int id, char name[]);
+void shared_print(string str, bool endLine);
+int broadcast_message(string message, int sender_id);
+int broadcast_message(int num, int sender_id);
+void end_connection(int id);
+void handle_client(int client_socket, int id);
+
+
